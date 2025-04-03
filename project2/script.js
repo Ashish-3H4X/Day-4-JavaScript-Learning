afc5324664be665261510cca48df175826291f16
Mr Rahul <ashish227087@gmail.com>
2025-04-03 10:34:14 +0530

    Accessing element from array

---
+++ project2/script.js
@@ -0,0 +1,4 @@
+let colors = ["Red" ,"Green","Blue"];
+  document.getElementById('demo1').innerHTML=colors[1];
+  document.getElementById('demo2').innerHTML=colors[0]
+  document.getElementById('demo3').innerHTML=colors[colors.length-1]
